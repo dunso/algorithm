10cd3da03bedec778aa0ccc9a1c52657eb0d5368
dunso <dunso@dun.so>
2017-08-18 23:47:16 +0800

    Format LeetCode0001.c

--- C_Cpp/LeetCode0001.c
+++ C_Cpp/LeetCode0001.c
@@ -1,75 +1,74 @@
-#include<stdio.h>
+#include <stdio.h>
 
 void QuickSort(int* aNums, int* aIndex, int aHead, int aTail)
 {
-	if (aHead >= aTail)
-	{
-		return;
-	}
-	int mLeft = aHead, mRight = aTail, mNumTmp = aNums[mLeft], mIndexTmp = aIndex[mLeft];
+    if (aHead >= aTail) {
+        return;
+    }
+    int mLeft = aHead, mRight = aTail, mNumTmp = aNums[mLeft], mIndexTmp = aIndex[mLeft];
 
-	while (mLeft < mRight)
-	{
-		while (mLeft < mRight && aNums[mRight] >= mNumTmp) mRight--;
-		if (mLeft < mRight)
-		{
-			aNums[mLeft] = aNums[mRight];
-			aIndex[mLeft] = aIndex[mRight];
-			mLeft++;
-		}
-		while (mLeft < mRight && aNums[mLeft] < mNumTmp) mLeft++;
-		if (mLeft < mRight) {
-			aNums[mRight] = aNums[mLeft];
-			aIndex[mRight] = aIndex[mLeft];
-			mRight--;
-		}
-	}
-	aNums[mLeft] = mNumTmp; aIndex[mLeft] = mIndexTmp;
+    while (mLeft < mRight) {
+        while (mLeft < mRight && aNums[mRight] >= mNumTmp)
+            mRight--;
+        if (mLeft < mRight) {
+            aNums[mLeft] = aNums[mRight];
+            aIndex[mLeft] = aIndex[mRight];
+            mLeft++;
+        }
+        while (mLeft < mRight && aNums[mLeft] < mNumTmp)
+            mLeft++;
+        if (mLeft < mRight) {
+            aNums[mRight] = aNums[mLeft];
+            aIndex[mRight] = aIndex[mLeft];
+            mRight--;
+        }
+    }
+    aNums[mLeft] = mNumTmp;
+    aIndex[mLeft] = mIndexTmp;
 
-	QuickSort(aNums, aIndex, aHead, mLeft - 1);
-	QuickSort(aNums, aIndex, mLeft + 1, aTail);
+    QuickSort(aNums, aIndex, aHead, mLeft - 1);
+    QuickSort(aNums, aIndex, mLeft + 1, aTail);
 }
 
 int* twoSum(int* nums, int numsSize, int target)
 {
-	if (numsSize < 2) {
-		return NULL;
-	}
+    if (numsSize < 2) {
+        return NULL;
+    }
 
-	int* mIndex = (int*)malloc(numsSize * sizeof(int));
+    int* mIndex = (int*)malloc(numsSize * sizeof(int));
 
-	for (int i = 0; i < numsSize; i++) {
-		mIndex[i] = i;
-	}
+    for (int i = 0; i < numsSize; i++) {
+        mIndex[i] = i;
+    }
 
-	QuickSort(nums, mIndex, 0, numsSize - 1);
+    QuickSort(nums, mIndex, 0, numsSize - 1);
 
-	int mLeft = 0, mRight = numsSize - 1;
+    int mLeft = 0, mRight = numsSize - 1;
 
-	if (mLeft == mRight) {
-		return NULL;
-	}
+    if (mLeft == mRight) {
+        return NULL;
+    }
 
-	int mNum = nums[mLeft] + nums[mRight];
+    int mNum = nums[mLeft] + nums[mRight];
 
-	while (mNum != target)
-	{
-		if (mNum > target && --mRight > mLeft) {}
-		else if (mNum < target && ++mLeft < mRight) {}
-		if (mLeft == mRight)
-		{
-			return NULL;
-		}
-		mNum = nums[mLeft] + nums[mRight];
-	}
+    while (mNum != target) {
+        if (mNum > target && --mRight > mLeft) {
+        } else if (mNum < target && ++mLeft < mRight) {
+        }
+        if (mLeft == mRight) {
+            return NULL;
+        }
+        mNum = nums[mLeft] + nums[mRight];
+    }
 
-	if (mNum == target) {
-		int* mResult = malloc(2 * sizeof(int));
-		mResult[0] = mIndex[mLeft];
-		mResult[1] = mIndex[mRight];
-		return mResult;
-	}
-	return NULL;
+    if (mNum == target) {
+        int* mResult = malloc(2 * sizeof(int));
+        mResult[0] = mIndex[mLeft];
+        mResult[1] = mIndex[mRight];
+        return mResult;
+    }
+    return NULL;
 }
 
 //int main()
