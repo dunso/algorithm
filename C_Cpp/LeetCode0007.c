0da081cd13847126dbf83277a5012db2ab700e8f
dunso <dunso@dun.so>
2017-08-31 23:55:21 +0800

    Modify LeetCode0007.c

--- C_Cpp/LeetCode0007.c
+++ C_Cpp/LeetCode0007.c
@@ -1,21 +1,22 @@
-#include<stdio.h>
-#include<limits.h>
+#include <limits.h>
+#include <stdio.h>
 
-int reverse(int x) {
-	long long mResult = 0;
-	while (x) {
-		mResult = mResult * 10 + x % 10;
+int reverse(int x)
+{
+    long long mResult = 0;
+    while (x) {
+        mResult = mResult * 10 + x % 10;
 
-		if (mResult > INT_MAX || mResult < INT_MIN) {
-			return 0;
-		}
-		x /= 10;
-	}
-	return mResult;
+        if (mResult > INT_MAX || mResult < INT_MIN) {
+            return 0;
+        }
+        x /= 10;
+    }
+    return mResult;
 }
 
-//int main() {
-//	printf("%d\n", reverse(1534236469));
-//	system("pause");
-//	return 0;
-//}
+int main()
+{
+    printf("%d\n", reverse(1534236469));
+    return 0;
+}
