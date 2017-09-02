a4a3efef738295c9f9801ae5819d529165c31a81
dunso <dunso@dun.so>
2017-09-02 23:56:43 +0800

    Modify LeetCode0008.c

--- C_Cpp/LeetCode0008.c
+++ C_Cpp/LeetCode0008.c
@@ -1,25 +1,25 @@
-#include<stdio.h>
-#include<limits.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int myAtoi(char * str)
+int myAtoi(char* str)
 {
-	long long mResult = 0;
+    long long mResult = 0;
 
-	while (isspace(*str++));
+    while (isspace(*str++))
+        ;
 
-	int mSign = *--str == '-' ? -1 : *str == '+' ? 1 : 0;
+    int mSign = *--str == '-' ? -1 : *str == '+' ? 1 : 0;
 
-	for (str = mSign ? ++str : str; isdigit(*str) && mResult <= INT_MAX; str++)
-	{
-		mResult = mResult * 10 + (*str - 48);
-	}
-	return mResult > INT_MAX ? mSign == -1 ? INT_MIN : INT_MAX : mSign == -1 ? -mResult : mResult;
+    for (str = mSign ? ++str : str; isdigit(*str) && mResult <= INT_MAX; str++) {
+        mResult = mResult * 10 + (*str - 48);
+    }
+    return mResult > INT_MAX ? mSign == -1 ? INT_MIN : INT_MAX : mSign == -1 ? -mResult : mResult;
 }
 
-//int main()
-//{
-//	char input[] = { '-','2','1','4','7','4','8','3','6','4','7','\0' };
-//	printf("%ld\n", myAtoi(input));
-//	system("pause");
-//	return 0;
-//}
+int main()
+{
+    char input[] = { '-', '2', '1', '4', '7', '4', '8', '3', '6', '4', '7', '\0' };
+    printf("%ld\n", myAtoi(input));
+    return 0;
+}
