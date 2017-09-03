0a48519f3ee448c86d46ca3637f36cc739af074a
dunso <dunso@dun.so>
2017-09-03 23:32:47 +0800

    LeetCode0009.c

--- C_Cpp/LeetCode0009.c
+++ C_Cpp/LeetCode0009.c
@@ -1,25 +1,27 @@
-#include<stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
-bool isPalindrome(int x) {
+bool isPalindrome(int x)
+{
 
-	if (x < 0)
-	{
-		return false;
-	}
+    if (x < 0) {
+        return false;
+    }
 
-	int mDigits = 1;
+    int mDigits = 1;
 
-	for (int num = x; (num /= 10) > 0; mDigits *= 10);
+    for (int num = x; (num /= 10) > 0; mDigits *= 10)
+        ;
 
-	for (; mDigits != 0 && x / mDigits == x % 10; x %= mDigits, mDigits /= 100, x /= 10);
+    for (; mDigits != 0 && x / mDigits == x % 10; x %= mDigits, mDigits /= 100, x /= 10)
+        ;
 
-	return x == 0;
+    return x == 0;
 }
 
-//int main() {
-//	int x = -1;
-//	printf("%d\n", isPalindrome(x));
-//	system("pause");
-//	return 0;
-//}
+int main()
+{
+    int x = -1;
+    printf("%d\n", isPalindrome(x));
+    return 0;
+}
