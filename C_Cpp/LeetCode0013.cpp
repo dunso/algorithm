28c00a5cd5adcbe1ebe8f875bd9c71984c218896
dunso <dunso@dun.so>
2017-09-06 23:49:32 +0800

    Modify LeetCode0013.cpp

--- C_Cpp/LeetCode0013.cpp
+++ C_Cpp/LeetCode0013.cpp
@@ -1,27 +1,29 @@
-#include<iostream>
-#include<string>
-#include<unordered_map>
+#include <iostream>
+#include <string>
+#include <unordered_map>
 
 using namespace std;
 
 class LeetCode13 {
 public:
-	int romanToInt(string s) {
-		static unordered_map<char, int> map = {
-			{ 'I', 1 },{ 'V', 5 },{ 'X', 10 },{ 'L', 50 },{ 'C', 100 },{ 'D', 500 },{ 'M', 1000 }
-		};
+    int romanToInt(string s)
+    {
+        static unordered_map<char, int> map = {
+            { 'I', 1 }, { 'V', 5 }, { 'X', 10 }, { 'L', 50 }, { 'C', 100 }, { 'D', 500 }, { 'M', 1000 }
+        };
 
-		int sum = map[s.back()];
+        int sum = map[s.back()];
 
-		for (int i = 0; i < s.length() - 1; i++) {
-			sum = map[s[i]] < map[s[i + 1]] ? sum - map[s[i]] : sum + map[s[i]];
-		}
-		return sum;
-	}
+        for (int i = 0; i < s.length() - 1; i++) {
+            sum = map[s[i]] < map[s[i + 1]] ? sum - map[s[i]] : sum + map[s[i]];
+        }
+        return sum;
+    }
 };
 
-int main() {
-	LeetCode13 leetCode13;
-	cout << leetCode13.romanToInt("MMMCMXCIX") << endl;
-	return 0;
+int main()
+{
+    LeetCode13 leetCode13;
+    cout << leetCode13.romanToInt("MMMCMXCIX") << endl;
+    return 0;
 }
