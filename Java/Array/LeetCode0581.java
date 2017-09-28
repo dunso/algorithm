7c1cf04712e41e8467d2525a4a9b3d9732f97ee3
dunso <dunso@dun.so>
2017-09-28 23:59:27 +0800

    Add LeetCode0581.java

---
+++ Java/Array/LeetCode0581.java
@@ -0,0 +1,39 @@
+package com.leetcode;
+
+public class LeetCode0581 {
+
+    public int findUnsortedSubarray(int[] nums)
+    {
+
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int right = 0;
+        int left = nums.length - 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= max) {
+                max = nums[i];
+                continue;
+            }
+            right = i;
+        }
+        for (int j = nums.length - 1; j >= 0; j--) {
+            if (nums[j] <= min) {
+                min = nums[j];
+                continue;
+            }
+            left = j;
+        }
+        return left >= right ? 0 : right - left + 1;
+    }
+
+    public static void main(String[] args)
+    {
+        LeetCode0581 leetcode = new LeetCode0581();
+        int[] nums = new int[] { 2, 6, 4, 8, 10, 9, 15 };
+        System.out.println(leetcode.findUnsortedSubarray(nums));
+    }
+}
