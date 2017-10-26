036c6f2eb8fbbebaa60e75a2bf172f951c607fde
dunso <dunso@dun.so>
2017-10-26 23:10:15 +0800

    Update LeetCode0217.java

--- Java/Array/LeetCode0217.java
+++ Java/Array/LeetCode0217.java
@@ -1,23 +1,23 @@
 public class LeetCode217 {
     public boolean containsDuplicate(int[] nums) {
 
-        int min=Integer.MAX_VALUE;
-        int max=Integer.MIN_VALUE;
-        for(int b: nums){
-            if(b<min){
-                min=b;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int b : nums) {
+            if (b < min) {
+                min = b;
             }
-            if(b>max){
-                max=b;
+            if (b > max) {
+                max = b;
             }
         }
 
-        boolean record[] = new boolean[max-min+1];
+        boolean record[] = new boolean[max - min + 1];
 
-        for(int num : nums){
-            if(record[num-min]){
+        for (int num : nums) {
+            if (record[num - min]) {
                 return true;
-            }else{
+            } else {
                 record[num - min] = true;
             }
         }
