bdedfa2d2748d7b45983e3956e597dc18bfe5421
dunso <dunso@dun.so>
2017-10-31 23:54:35 +0800

    Update LeetCode0041.c

--- C/Array/LeetCode0041.c
+++ C/Array/LeetCode0041.c
@@ -1,37 +1,31 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int firstMissingPositive(int * nums, int numsSize)
+int firstMissingPositive(int* nums, int numsSize)
 {
-	for(int i = 0; i< numsSize; i++)
-	{
-		if(nums[i] <= 0 || nums[i] > numsSize || nums[i] == i + 1)
-		{
-			continue;
-		}
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] <= 0 || nums[i] > numsSize || nums[i] == i + 1) {
+            continue;
+        }
 
-		if(nums[nums[i] - 1] != nums[i])
-		{
-			int tmp = nums[nums[i] - 1];
-			nums[nums[i] - 1] = nums[i];
-			nums[i] = tmp;
-			i--;
-		}
-	}
+        if (nums[nums[i] - 1] != nums[i]) {
+            int tmp = nums[nums[i] - 1];
+            nums[nums[i] - 1] = nums[i];
+            nums[i] = tmp;
+            i--;
+        }
+    }
 
-	for(int i = 0; i< numsSize; i++)
-	{
-		if(nums[i] != i +1)
-		{
-			return i + 1;
-		}
-	}
-	return numsSize + 1;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != i + 1) {
+            return i + 1;
+        }
+    }
+    return numsSize + 1;
 }
 
 int main()
 {
-	int nums[] = { 3, 4, -1, 1 };
-	printf("%d", firstMissingPositive(nums, 4));
-	system("pause");
-	return 0;
+    int nums[] = { 3, 4, -1, 1 };
+    printf("%d", firstMissingPositive(nums, 4));
+    return 0;
 }
