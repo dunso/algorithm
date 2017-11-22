84d3490fd2c12b05bc38186a7f03930460b2d5db
dunso <dunso@dun.so>
2017-11-22 23:43:54 +0800

    Add LeetCode0021.java
    Add LeetCode0021.java

---
+++ Java/Linked/LeetCode0021.java
@@ -0,0 +1,38 @@
+public class LeetCode0021 {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2)
+    {
+
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+
+        ListNode head = new ListNode(0);
+        ListNode p = head;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            }
+            else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+
+        p.next = l1 == null ? l2 : l1;
+
+        return head.next;
+    }
+}
