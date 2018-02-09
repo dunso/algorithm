e14d6af161c366b50066ae6cae249f12f6c89587
dunso <dunso@dun.so>
2018-02-10 01:29:23 +0800

    Add LeetCode0063.java

---
+++ Java/DP/LeetCode0063.java
@@ -0,0 +1,52 @@
+
+public class LeetCode0063 {
+	public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+		if (obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0) {
+			return 0;
+		}
+
+		if (obstacleGrid[0][0] == 1) {
+			return 0;
+		}
+
+		int m = obstacleGrid.length;
+		int n = obstacleGrid[0].length;
+
+		int[][] dp = new int[m][n];
+
+		dp[0][0] = 1;
+
+		for (int i = 1; i < m; i++) {
+			if (obstacleGrid[i][0] == 1) {
+				dp[i][0] = 0;
+			} else {
+				dp[i][0] = dp[i - 1][0];
+			}
+		}
+
+		for (int j = 1; j < n; j++) {
+			if (obstacleGrid[0][j] == 1) {
+				dp[0][j] = 0;
+			} else {
+				dp[0][j] = dp[0][j - 1];
+			}
+		}
+
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				if (obstacleGrid[i][j] == 1) {
+					dp[i][j] = 0;
+				} else {
+					dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+				}
+			}
+		}
+		return dp[m - 1][n - 1];
+	}
+
+	public static void main(String[] args) {
+		LeetCode0063 leetcode = new LeetCode0063();
+		int[][] obstacleGrid = new int[][] { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 0 } };
+		System.out.println(leetcode.uniquePathsWithObstacles(obstacleGrid));
+	}
+}
