7ef6ea67ccc9e5384634d42a685189f83fcc3710
dunso <dunso@dun.so>
2018-02-12 03:02:58 +0800

    Add LeetCode0064.java

---
+++ Java/DP/LeetCode0064.java
@@ -0,0 +1,36 @@
+
+public class LeetCode0064 {
+	public int minPathSum(int[][] grid) {
+		if (grid == null || grid.length == 0 || grid[0].length == 0) {
+			return 0;
+		}
+
+		int m = grid.length;
+		int n = grid[0].length;
+
+		int[][] dp = new int[m][n];
+
+		dp[0][0] = grid[0][0];
+
+		for (int i = 1; i < m; i++) {
+			dp[i][0] = dp[i - 1][0] + grid[i][0];
+		}
+
+		for (int j = 1; j < n; j++) {
+			dp[0][j] = dp[0][j - 1] + grid[0][j];
+		}
+
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+			}
+		}
+		return dp[m - 1][n - 1];
+	}
+
+	public static void main(String[] args) {
+		LeetCode0064 leetcode = new LeetCode0064();
+		int[][] grid = new int[][] { { 1, 3, 1 }, { 1, 5, 1 }, { 4, 2, 1 } };
+		System.out.println(leetcode.minPathSum(grid));
+	}
+}
