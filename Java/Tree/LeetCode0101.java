a712955ba206a444047f4307266d0352058bbd75
anikahuo <kaiyue.huo@gmail.com>
2018-03-13 23:38:21 +0800

    update LeetCod0101e.java

--- Java/Tree/LeetCode0101.java
+++ Java/Tree/LeetCode0101.java
@@ -15,12 +15,15 @@ public class LeetCode0101 {
 	        return isSymmetricTree(root.left,root.right);
 	    }
 	    private boolean isSymmetricTree(TreeNode p, TreeNode q){
-	        if(p==null && q==null)
-	            return true;
-	        if((p==null && q!=null)||(p!=null && q==null))
-	            return false;
-	        if(p.val!= q.val)
-	            return false;
+	        if(p==null && q==null) {
+	        	return true;
+	        }
+	        if((p==null && q!=null)||(p!=null && q==null)) {
+	        	return false;
+	        }   
+	        if(p.val!= q.val) {
+	        	return false;  	
+	        }
 	        return isSymmetricTree(p.left,q.right) && isSymmetricTree(p.right, q.left);
 	    }
 }
