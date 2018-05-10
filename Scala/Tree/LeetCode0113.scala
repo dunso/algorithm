29795b99f7df96a172e283f60bf13c80c6a0e3d2
dunso <dunso@dun.so>
2018-05-11 00:12:42 +0800

    fix the bug of  LeetCode0113.scala

--- Scala/Tree/LeetCode0113.scala
+++ Scala/Tree/LeetCode0113.scala
@@ -1,4 +1,3 @@
-
 class TreeNode(var _value: Int) {
   var value: Int = _value
   var left: TreeNode = null
@@ -9,14 +8,14 @@ object LeetCode0113 {
   def pathSum(root: TreeNode, sum: Int): List[List[Int]] = {
 
     if (root == null) return List();
-    if (root != null && root.left == null && root.right == null &&  sum == root.value) {
+    if (root != null && root.left == null && root.right == null && sum == root.value) {
       return List(List(sum));
     }
     var path: List[List[Int]] = List();
-    if (root.left != null && sum != root.value) {
+    if (root.left != null) {
       path :::= pathSum(root.left, sum - root.value).map(root.value :: _);
     }
-    if (root.right != null && sum != root.value) {
+    if (root.right != null) {
       path :::= pathSum(root.right, sum - root.value).map(root.value :: _);
     }
     path;
